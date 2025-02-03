6f329966b56c96048b1ea460c261a38564bf042b
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-03 11:36:21 +0800

    feat: add validation for country input

---
+++ src/form-validation/country-validation.js
@@ -0,0 +1,22 @@
+const countryInput = document.getElementById("country");
+const countryError = document.querySelector("#country + span.error");
+const form = document.getElementById("form");
+
+function showCountryError() {
+    if (countryInput.value === "-") {
+        countryError.textContent = "Please select a country";
+        countryError.className = "error active";
+    }
+}
+
+export function attachCountryEventListener() {
+    form.addEventListener("submit", (event) => {
+        if (countryInput.value !== "-") {
+            countryError.textContent = "";
+            countryError.className = "error";
+        } else {
+            event.preventDefault();
+            showCountryError();
+        }
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./style.css";
 import { attachEmailEventListener } from "./form-validation/email-validation";
+import { attachCountryEventListener } from "./form-validation/country-validation";
 
 attachEmailEventListener();
+attachCountryEventListener();
